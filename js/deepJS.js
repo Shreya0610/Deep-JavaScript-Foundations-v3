7cf001c34c43104906e80f9b3cc2adfbe6498441
Shreya Sharma <sshreyasharma53@gmail.com>
2021-09-29 20:56:34 +0530

    initial commit

--- js/deepJS.js
+++ js/deepJS.js
@@ -5,3 +5,9 @@ x = x + 1; //51
 var y = "5";
 y++; //5
 y; //6
+
+// everything is an object is a saying for js but its wrong. Many things behave as object but are NOT OBJECTS.
+
+//bigint
+var x = 42n;
+typeof x;
